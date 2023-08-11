f34f53eaf9d514f4205cc283220c2981978615d4
chenwenjie2 <chenwenjie2@yy.com>
2023-08-11 18:38:36 +0800

    完善了部分匹配规则

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 import sys
 import re
 
+
+start_mat_re = r"(.+Constraints\s*[(|\s]\{.*)"
 def searchFiles():
     # 搜索文件夹路径
-    # folder_path = '/Users/kino/YY-Work/Direct-Haokan/YYULiveSDK-ios/yyulivesdk'
-    folder_path = '/Users/kino/YY-Work/Direct-Haokan/YYULiveSDK-ios/yyulivesdk/SupportBusiness/Channel/Modules/SachetTeamModule'
+    folder_path = '/Users/kino/YY-Work/Direct-Haokan/YYULiveSDK-ios/yyulivesdk'
+    # folder_path = '/Users/kino/YY-Work/Direct-Haokan/YYULiveSDK-ios/yyulivesdk/SupportBusiness/Channel/Modules/ChatModule/Message/Announcement/'
     print(f"正在搜索{folder_path}内的文件...")
     # 要替换的字符串和替换后的字符串
     replaceMap = {
@@ -38,8 +40,6 @@ def searchFiles():
         'mas_safeAreaLayoutGuideBottom': 'snp.safeAreaLayoutGuideBottom',
         'mas_safeAreaLayoutGuideLeft': 'snp.safeAreaLayoutGuideLeft',
         'mas_safeAreaLayoutGuideRight': 'snp.safeAreaLayoutGuideRight',
-        'make?': 'make',
-        'maker?': 'maker',
         'mas_equalTo()': 'equalTo',
         'equalTo()': 'equalTo',
         'mas_greaterThanOrEqualTo()': 'greaterThanOrEqualTo',
@@ -57,6 +57,12 @@ def searchFiles():
         'centerX().': 'centerX.',
         'centerY().': 'centerY.',
         'baseline().': 'baseline.',
+    }
+
+    in_block_repalce_map = {
+        'make?': 'make',
+        'maker?': 'maker',
+        '$0?': '$0'
     }
 
     pattern_reps = ['inset',
@@ -77,30 +83,7 @@ def searchFiles():
                 with open(file_path, 'r') as f:
                     contentLines = f.readlines()
 
-                new_contens = []
-                for line in contentLines:
-                    # 替换字符串并写入文件
-                    # new_content = content.replace(old_str, new_str)
-                    # 遍历替换映射表中的所有键和值，并将文本中的匹配项替换为目标字符串
-                    for old_str, new_str in replaceMap.items():
-                        line = line.replace(old_str, new_str)
-
-                    # 二级修改
-                    # 要匹配的正则表达式
-                    for par_comp in pattern_reps:
-                        pattern = r'\?\.{}\(\)'.format(par_comp)
-                        # 使用正则表达式进行匹配和替换
-                        replacement = "." + par_comp
-                        line = re.sub(pattern, replacement, line)
-
-                    # 如果以.equalTo结尾
-                    if line.endswith('.equalTo'):
-                        # 将.equalTo替换为.equalTo(snp.equalTo)
-                        line = line.replace('.equalTo', '.equalToSuperview()')
-                    new_contens.append(line)
-
-                # 拼回字符串
-                content = ''.join(new_contens)
+                content = find_and_replace(contentLines, in_block_repalce_map, pattern_reps, replaceMap)
 
                 with open(file_path, 'w') as f:
                     f.write(content)
@@ -108,9 +91,66 @@ def searchFiles():
                 print(f'{file} 替换完成')
 
 
+
+def test_check():
+    line = "view.videoProvider.playerView.mas_makeConstraints { make in\n"
+    mark_start = False
+    # 判断布局开始和结束
+    match = re.findall(start_mat_re, line)
+    if match:
+        mark_start = True
+        print("命中")
+
+
+    str = """
+        inputCountLabel.mas_makeConstraints { maker in
+            maker?.bottom.trailing().offset()(-10)
+            maker?.height.equalTo()(24)
+        }
+    """
+
+
+def find_and_replace(contentLines, in_block_repalce_map, pattern_reps, replaceMap):
+    new_contens = []
+    mark_start = False
+    for line in contentLines:
+        # 判断布局开始和结束
+        match = re.findall(start_mat_re, line)
+        if match:
+            mark_start = True
+
+        if mark_start:
+            if line.endswith("}\n") or line.endswith("})\n"):
+                mark_start = False
+
+        # 遍历替换映射表中的所有键和值，并将文本中的匹配项替换为目标字符串
+        for old_str, new_str in replaceMap.items():
+            line = line.replace(old_str, new_str)
+
+        if mark_start:
+            for old_str, new_str in in_block_repalce_map.items():
+                line = line.replace(old_str, new_str)
+
+        # 二级修改
+        # 要匹配的正则表达式
+        for par_comp in pattern_reps:
+            pattern = r'\?\.{}\(\)'.format(par_comp)
+            # 使用正则表达式进行匹配和替换
+            replacement = "." + par_comp
+            line = re.sub(pattern, replacement, line)
+
+        # 如果以.equalTo结尾
+        if line.endswith('.equalTo\n'):
+            # 将.equalTo替换为.equalTo(snp.equalTo)
+            line = line.replace('.equalTo', '.equalToSuperview()')
+        new_contens.append(line)
+    # 拼回字符串
+    content = ''.join(new_contens)
+    return content
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    test_check()
     searchFiles()
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
